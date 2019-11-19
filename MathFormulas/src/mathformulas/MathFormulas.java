f3a740c78b3c9b7abe977f775fb29383526e82ea
Jcmcn <57957945+Jcmcn@users.noreply.github.com>
2019-11-19 12:47:14 -0500

    Created methods to calculate x,y,z,w and v

--- MathFormulas/src/mathformulas/MathFormulas.java
+++ MathFormulas/src/mathformulas/MathFormulas.java
@@ -10,7 +10,17 @@ public class MathFormulas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Put your math formula implementations here
+        double a = 1.0;
+        double b = 2.0;
+        double m = 3.0;
+        
+        double x = (a-5.0)*(b+4.0);
+        double y = m*x + b;
+        double z = 4.0*(2.0*x+7.0)*(3.0*x-2.0);
+        double w = (m-4.0*x)/(7.0-y);
+        double v = (a*x*2.0+b*x+z)/(m*x-w);
+        System.out.println("X is equal to " + x + "\n" + "Y is equal to " + y + "\n" + "Z is equal to " + z + "\n" + "W is equal to " + w + "\n" + "V is equal to " + v);
+        
     }
     
 }
